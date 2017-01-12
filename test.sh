598180ac6fb40048d882d41cf5fe602f4ca1b2ab
ZanyXDev <zanyxdev@gmail.com>
2017-01-12 06:09:14 +0300

    remove persistent data

--- test.sh
+++ test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 USER=`whoami`
 
-mkdir -p `pwd`/persistent_data/
-chown -R $USER:$USER `pwd`/persistent_data/
+mkdir -p ~/persistent_data/
+chown -R $USER:$USER ~/persistent_data/
+
+XAUTH=~/persistent_data/.docker.xauth
+echo "Building $XAUTH"
+touch $XAUTH
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
 docker run -ti \
     --privileged \
     --net=host \
     -e DISPLAY=unix$DISPLAY \
+    -e XAUTHORITY=$XAUTH \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v `pwd`/persistent_data:/opt/persistent_data \
+    -v /dev/bus/usb:/dev/bus/usb \
     dev-test
+#    -v ~/persistent_data:/opt/persistent_data \
+
 
