327155f25b126baf811b9d71e5aa439ed963ba76
ZanyXDev <zanyxdev@gmail.com>
2017-01-12 04:12:39 +0300

    change startup script

--- test.sh
+++ test.sh
@@ -2,13 +2,13 @@
 USER=`whoami`
 
 mkdir -p `pwd`/persistent_data/
-chown -R $USER:USER `pwd`/persistent_data/*
+chown -R $USER:$USER `pwd`/persistent_data/
 
 docker run -ti \
     --privileged \
     --net=host \
     -e DISPLAY=unix$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v `pwd`/persistent_data/downloads:/developer/persistent_data \
-    zanyxdev/dev-java-test
+    -v `pwd`/persistent_data:/opt/persistent_data \
+    dev-test
 
