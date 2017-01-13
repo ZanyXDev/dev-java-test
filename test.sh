9e8a25d3014ee2495c22cb58102316e9c18cb5be
ZanyXDev <zanyxdev@gmail.com>
2017-01-13 06:48:21 +0300

    add rm option

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo "Building $XAUTH"
 touch $XAUTH
 xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
 
-docker run -ti \
+docker run -it --rm \
     --privileged \
     --net=host \
     -e DISPLAY=unix$DISPLAY \
@@ -18,4 +18,6 @@ docker run -ti \
     -v /dev/bus/usb:/dev/bus/usb \
     -v ~/persistent_data:/opt/persistent_data \
     -v ~/.android-studio-docker:/home/developer/.AndroidStudio2.2 \
-    dev-test
+    zanyxdev/dev/java-test
+
+
